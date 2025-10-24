f2a3127659a1f94af3c15d56e4a44852ac6f25bb
giaminh <daiminhmap@gmail.com>
2025-10-24 20:26:16 +0700

    Vẫn đang làm detail ^_^

--- spk_or_something/js/detail.js
+++ spk_or_something/js/detail.js
@@ -2,14 +2,38 @@ const detailContainer = document.querySelector('.detail')
 
 const getDetailMusic = async () =>{
     const path = new URLSearchParams(window.location.search);
-    const musicId = path.get('id');
-    const response = await fetch('../data.json');
+    const musicId = path.get('id'); 
+    const response = await fetch('./data/music.json');
     const data = await response.json();
     const findMusicId = data.find(item => item.id.toString()=== musicId.toString())
+    // console.log(findMusicId);
 
     detailContainer.innerHTML = `
-    
+     <div class="container detail">
+        <h1>${findMusicId.title}</h1>
+        <div class="song-info">
+            <p class="lead">Artist: <strong>${item.artist}</strong></p>
+            <img src="${findMusicId.img}" class="song-cover" alt="${findMusicId.title} by ${findMusicId.artist}">
+            <audio controls>
+                <source src="${findMusicId.mp3}" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+        </div>
+
+        <div class="song-meta">
+            <h5>About this song</h5>
+            <p><strong>Genre:</strong>${findMusicId.genre}</p>
+            <p><strong>Album:</strong> ${findMusicId.album}p>
+            <p><strong>Awards:</strong> ${findMusicId.awards} </p>
+            <p><strong>Message:</strong> ${findMusicId.message}</p>
+        </div>
+
+        <div class="lyrics">
+            <h5>Lyrics : </h5>
+            <p>${findMusicId.lyrics}</p>
+        </div>
+    </div>
     
     `
 }
-getDetailMusic
+getDetailMusic();
--- spk_or_something/js/playlist.js
+++ spk_or_something/js/playlist.js
@@ -7,14 +7,14 @@ const getData = async () => {
     if (data) {
         musiclist.innerHTML = data.map(item => {
             return `
-            <div class="song-item col-3 gap-3 p-0">
+            <div  class="song-item col-3 gap-3 p-0">
                 <div class="card h-100">
                     <img src="${item.img}" class="card-img-top w-100 h-50" alt="${item.title} By : ${item.artist}">
                     <div class="card-body">
                     <h1></h1>
                         <h5 class="card-title">${item.title}</h5>
                         <p class="card-text">By : ${item.artist}</p>
-                        <a href="./song_detail_tyler.html" class="btn btn-success btn-lg w-100 the-shadow">Listen</a>
+                        <a href="./song_detail_tyler.html?id=${item.id}" class="btn btn-success btn-lg w-100 the-shadow">Listen</a>
                     </div>
                 </div>
             </div>
